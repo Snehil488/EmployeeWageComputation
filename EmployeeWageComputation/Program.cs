628ae11b73003afa3034fb7ae157a14c9f96c5f8
Snehil <snehilverma488@gmail.com>
2020-09-28 13:32:22 +0530

    [Snehil] Refactor : Employee Monthly Wage Calculation

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,23 +7,22 @@ namespace EmployeeWageComputation
                 
                public const int IS_FULL_TIME = 1;
                public const int IS_PART_TIME = 2;
+               public const int EMP_RATE_PER_HOUR = 20;
+               public const int NUMBER_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
                 Console.WriteLine("Welcome to Employee Wage Computation.");
-                int EMP_RATE_PER_HOUR = 20;
                 int empHrs = 0;
                 int empWage = 0;
+                int totalEmpWage = 0;
 
-
+            for (int day = 0; day < NUMBER_OF_WORKING_DAYS; day++)
+            {
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
 
-                 if (empCheck == IS_FULL_TIME || empCheck == IS_PART_TIME)
-                    Console.WriteLine("Employee is Present");
-                 else
-                    Console.WriteLine("Employee is absent");
 
-                switch(empCheck)
+                switch (empCheck)
                 {
                     case IS_PART_TIME:
                         empHrs = 4;
@@ -35,10 +34,13 @@ namespace EmployeeWageComputation
                         empHrs = 0;
                         break;
                 }
-            
+
 
                 empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
                 Console.WriteLine("Emp Wage: " + empWage);
+            }
+            Console.WriteLine("Total Employee Wage is :"+totalEmpWage);
         }
     }
 }
