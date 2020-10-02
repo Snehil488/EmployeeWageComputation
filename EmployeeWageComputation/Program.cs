54383b0fb11bf4a12321280ead41e59362ae75c9
Snehil <snehilverma488@gmail.com>
2020-10-02 18:13:04 +0530

    nil

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -14,6 +14,7 @@ namespace EmployeeWageComputation
                     Console.WriteLine("Employee is Present");
                  else
                     Console.WriteLine("Employee is absent");
+//updated 
         }
     }
 }
